ac3fa8cfc1340fcff682a8e007c07f563e11d1d8
RouanLaporal <rouan.laporal@outlook.com>
2021-11-27 10:50:54 +0100

    création de la fonction apiwithkey()

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,40 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Etabli la connexion à l'api, récupère les données, détermine la plus grosse croissance sur les dernières 24h et ajoute une trace de cette crypto dans la BD
+     * @return view return la vue crypto et lui fourni les variables à afficher
+     */
+    public function apiWithKey(){
+        $client = new Client;
+        $url = "api.coincap.io/v2/assets";
+        
+        $headers = [
+            'api-key' => '5f87c432-9344-4350-b91d-32999d89bf1d'
+        ];
+
+        $response = $client->request('GET', $url, [
+            'headers' => $headers,
+            'verify' => false,
+        ]);
+
+        $responseBody = json_decode($response->getBody());
+        $maxGrowth =  floatval($responseBody->data[0]->changePercent24Hr);
+        foreach ($responseBody->data as $response => $value){
+            if(floatval($value->changePercent24Hr) > $maxGrowth){
+                $maxGrowth = floatval($value->changePercent24Hr);
+                $maxName = $value->name;
+                $maxValue = floatval($value->priceUsd);
+            }
+        }
+        $data = array('cryptoName' => $maxName, "price"=>$maxValue, "growth" => $maxGrowth);
+        DB::table('cryptos')->insert($data);
+
+        return view('crypto', compact([
+            'maxName' => $maxName,
+            'maxGrowth' => substr($maxGrowth,0,6),
+            'maxValue' => substr($maxValue,0,8),
+        ]));
+    }
 }
